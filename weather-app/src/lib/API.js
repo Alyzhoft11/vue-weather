71201bc74d99379f1de0008800398e65167ebb37
Alex <alyzhoft11@gmail.com>
2018-09-26 21:16:58 -0500

    Finished Weather App

--- weather-app/src/lib/API.js
+++ weather-app/src/lib/API.js
@@ -1,13 +1,26 @@
 const proxy = 'http://cors-anywhere.herokuapp.com/';
 const API_KEY = 'f35641ff84cb8fa6290d3292d06509d8';
-const location = '37.8267,-122.4233';
-const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/${location}`;
+const WEATHER_API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/`;
+const GEO_API_URL = `${proxy}https://darksky.net/geo?q=`
+const ADDRESS_API_URL = `${proxy}https://darksky.net/rgeo?hires=1`
 
-function getForecast(){
-  return fetch(API_URL)
+function getCoordinates(location) {
+  return fetch(`${GEO_API_URL}${location}`)
+  .then(response => response.json())
+}
+
+function getForecast(lat, log){
+  return fetch(`${WEATHER_API_URL}${lat},${log}`)
     .then(response => response.json())
 }
 
+function getAddress(lat, log){
+  return fetch((`${ADDRESS_API_URL}&lat=${lat}&lon=${log}`))
+    .then(response => response.json())
+} 
+
 export default {
-  getForecast
+  getForecast,
+  getCoordinates,
+  getAddress
 };
--- weather-app/src/views/Home.vue
+++ weather-app/src/views/Home.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="home">
     <div class="row">
-      <div class="col-8 offset-2 text-center">
+      <div class="col-8 offset-2">
+        <div class="input-group mb-3">
+          <input v-model="location" type="text" class="form-control" placeholder="Enter Location">
+          <div class="input-group-append">
+            <button @click="updateLocation" class="btn btn-outline-secondary" type="button">Search</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-8 offset-2 text-center" v-if="forecast">
         <div class="card text-white bg-secondary mb-3">
-          <div class="card-header">Current Weather</div>
+          <div class="card-header">{{address}}</div>
           <div class="card-body">
             <h4 class="card-title">{{forecast.currently.summary}}</h4>
-            <div class="card-text">
-              {{forecast.currently.icon}}
+            <div class="card-text icon-temp">
+              <span class="emoji">{{icons[forecast.currently.icon]}}</span>
               {{forecast.currently.temperature}} &#8457;
             </div> 
             <div class="card-text">
-              {{forecast.currently.precipProbability}} %
+              {{forecast.currently.precipProbability}} % of Precipitation
             </div>
           </div>
         </div>
       </div>
     </div>
-    <pre>{{forecast}}</pre>
   </div>
 </template>
 
@@ -28,15 +35,62 @@ export default {
   name: 'home',
   data() {
     return {
-      forecast: {}
+      location: localStorage.location || '',
+      forecast: null,
+      address: localStorage.address || '',
+      icons: {
+        'clear-day': '🌞',
+        'clear-night': '🌃',
+        rain: '☔',
+        snow: '🌨',
+        sleet: '❄',
+        wind: '💨',
+        fog: '🌁',
+        cloudy: '☁',
+        'partly-cloudy-day': '🌤',
+        'partly-cloudy-night': '🌌'
+      }
     };
   },
   mounted() {
-    API.getForecast()
-      .then(result => {
-        console.log(result);
-        this.forecast = result
-      });
+    this.loadWeather(localStorage.lat || '37.8267', localStorage.log || '-122.4233');
+  },
+  methods: {
+    loadWeather(lat, log) {
+      localStorage.lat = lat;
+      localStorage.log = log;
+      API.getAddress(lat, log)
+        .then(result => {
+            this.address = [result.name, result.street].join(' ');
+            localStorage.address = this.address;
+        })
+      API.getForecast(lat, log)
+        .then(result => {
+          this.forecast = result;
+        });
+    },
+    updateLocation() {
+      localStorage.location = this.location
+      API.getCoordinates(this.location)
+        .then(result => {
+          this.loadWeather(result.latitude, result.longitude)      
+        })
+    }
   },
 };
 </script>
+
+<style>
+  body {
+    font-size: 2em;
+  }
+
+  .icon-temp {
+    font-size: 2em;
+  }
+
+  .emoji {
+    font-size: 2em;
+  }
+</style>
+
